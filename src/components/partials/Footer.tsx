9e15e28203530a85bf377610425756c4cada1bd2
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-06-28 21:48:12 +0700

    Footer

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
     return (
-        <div data-scroll-container className='w-full h-[460px] text-white bg-[#101010] mt-[-500px] z-50'>
+        <div data-scroll-container className='w-full h-[460px] text-white bg-[#101010] mt-[-500px] z-50 text-center'>
             Footer
         </div>
     )
